369f5a747fbbc792cfbd69af609d974a32429006
spoorthiuk <spoorthiuday27@gmail.com>
2025-01-21 10:42:48 -0700

    intiial commit

---
+++ src/test/java/com/example/pickleball_score_tracker/PickleballScoreTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pickleball_score_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickleballScoreTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
