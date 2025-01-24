6db84c6c8132e7a0d7879494082ce77018a3cc87
spoorthiuk <spoorthiuday27@gmail.com>
2025-01-24 12:02:52 -0700

    Add Application Configuration and JwtAuthenticationFilter

--- src/main/java/com/example/pickleball_score_tracker/services/JwtService.java
+++ src/main/java/com/example/pickleball_score_tracker/services/JwtService.java
@@ -59,7 +59,12 @@ public class JwtService {
                 .compact();
     }
 
-    public boolean IsTokenExpired(String token){
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
 
